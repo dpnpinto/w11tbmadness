bb1b2eb611978bb56d28bce19fe7962ff0025efe
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-07-15 16:55:02 +0000

    Update removebs.bat

--- removebs.bat
+++ removebs.bat
@@ -1,3 +1,4 @@
 Get-AppxPackage Microsoft.YourPhone -AllUsers | Remove-AppxPackage
 Get-AppxPackage "*xbox*" | Remove-AppxPackage
 Get-AppxPackage *Windows.DevHome* | Remove-AppxPackage
+Get-AppxPackage *CrossDevice* | Remove-AppxPackage
